1a7f250832ff15102d2cd1ae14c30108d7c71951
gsuaya <gsuaya@udesa.edu.ar>
2025-11-07 11:02:36 -0300

    Ejercicio 2 clase 11

---
+++ Clase11/Ejercicio2/index.js
@@ -0,0 +1,30 @@
+
+fetch('https://rickandmortyapi.com/api/character')
+
+    .then (function(response){
+        return response.json();
+})
+
+    .then(function(data){
+        console.log(data.results);
+
+    let dom = document.querySelector(".characterList")  
+    let characters = ''
+    for (let i=0; i<data.results.length; i++){
+         characters += 
+
+        ` <article>
+            <img src='${data.results[i].image}' alt="" ></img>
+            <p>Name: ${data.results[i].name} </p>
+            <p>Status: ${data.results[i].status} </p>
+
+         </article>`
+    }
+    dom.innerHTML = characters
+    console.log(characters)
+    console.log(data.results.length)
+    })
+
+    .catch(function(error){
+        console.log(error)
+    })
